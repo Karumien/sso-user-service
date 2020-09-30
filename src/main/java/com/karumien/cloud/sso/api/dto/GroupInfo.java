a63cabeb64f7c7430c54ce8dca959369bcd92ba3
Ivo Smajstrla <ext-ivo.smajstrla@eurowag.com>
2020-09-30 09:41:35 +0200

    P538-890 getting modules from json instead of keycloak

---
+++ src/main/java/com/karumien/cloud/sso/api/dto/GroupInfo.java
@@ -0,0 +1,34 @@
+package com.karumien.cloud.sso.api.dto;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class GroupInfo {
+	
+	  @JsonProperty("moduleId")
+	  private String moduleId;
+
+	  @JsonProperty("name")
+	  private String name;
+	  
+	  @JsonProperty("serviceId")
+	  private String serviceId;
+
+	  @JsonProperty("translation")
+	  private String translation;
+
+	  @JsonProperty("businessPriority")
+	  private Integer businessPriority;
+
+	  @JsonProperty("groups")
+	  private List<GroupInfo> groups;
+	  
+	  @JsonProperty("attributes")
+	  private Map<String, List<String>> attributes;
+
+}
